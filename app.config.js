60f23a757b6c4024d95deb65956eaf5b63c05537
RizkiFortius <rizki@fortiushris.com>
2024-08-02 02:59:05 +0700

    New: EAS Configuration

---
+++ app.config.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+
+const PACKAGES = process.env.PACKAGE_NAME;
+
+export default {
+  expo: {
+    name: "coin-quest",
+    slug: "coin-quest",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "coinquest",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/images/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    runtimeVersion: {
+      policy: "appVersion",
+    },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: PACKAGES,
+      buildNumber: "100000",
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      package: PACKAGES,
+      versionCode: 100000,
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: ["expo-router"],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      eas: {
+        projectId: "c12540eb-aa6d-4a53-b4e6-b64b3c80f6fc",
+      },
+    },
+  },
+};
